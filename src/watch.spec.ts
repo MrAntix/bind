7a6b15d1dc85c7ed46fd13aecb0009143fd74ce1
Anthony Johnston <aj@antix.co.uk>
2019-08-05 11:36:38 +0100

    feat(watch): watch setters on an object

---
+++ src/watch.spec.ts
@@ -0,0 +1,64 @@
+import { watch } from './watch';
+
+describe('watch', () => {
+  const prop = 'PROPERTY';
+  const value = 'VALUE';
+  let o;
+
+  beforeEach(() => {
+    o = {};
+  });
+
+  it('watches for change', () => {
+    let counter = 0;
+    watch(o, prop, () => {
+      counter++;
+    });
+
+    o[prop] = value;
+    o[prop] = value;
+
+    expect(counter).toBe(2);
+  });
+
+  it('watches for change once', () => {
+    let counter = 0;
+    watch(
+      o,
+      prop,
+      () => {
+        counter++;
+      },
+      true
+    );
+
+    o[prop] = value;
+    o[prop] = value;
+
+    expect(counter).toBe(1);
+  });
+
+  it('unwatches and keeps set value', () => {
+    const unwatch = watch(o, prop, () => {});
+
+    o[prop] = value;
+    unwatch();
+
+    expect(o[prop]).toBe(value);
+  });
+
+  it('can nest watches', () => {
+    let counter = 0;
+    watch(o, prop, () => {
+      counter++;
+    });
+    watch(o, prop, () => {
+      counter++;
+    });
+
+    o[prop] = value;
+    o[prop] = value;
+
+    expect(counter).toBe(4);
+  });
+});
