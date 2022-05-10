674c37be63bdbbdba195325a8017b463d73e874c
Anthony Johnston <aj@antix.co.uk>
2022-05-10 12:54:23 +0100

    fix(index): add missing refs

--- src/getBindings.ts
+++ src/getBindings.ts
@@ -7,6 +7,7 @@ import { IBinding } from './IBinding';
  * @param exp Expression used to find attributes to bind to
  * @param element Element to be bound
  * @param attributeNames names of attibutes on the element
+ * @returns array of bindings
  */
 export function getBindings(
   exp: RegExp,
